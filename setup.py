2e8c69f383d0246a2b406bb3f52ca59b510640ed
Dan Myung <dmyung@dimagi.com>
2011-04-20 13:12:33 -0400

    updating readme, adding setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name='couchdebugpanel',
+    version='0.0.1',
+    description='A CouchDBkit panel for the django debug toolbar',
+    author='Dimagi',
+    author_email='information@dimagi.com',
+    url='http://www.dimagi.com/',
+    packages = find_packages(exclude=['*.pyc']),
+    install_requires = [
+        "restkit", "couchdbkit", "django-debug-toolbar",
+    ],
+)
+

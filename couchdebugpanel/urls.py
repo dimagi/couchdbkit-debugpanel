9df0710b0c4ffd6202214441732bc3336051a240
Dan Myung <dmyung@dimagi.com>
2011-05-01 18:12:05 -0400

    fixing view typo

--- couchdebugpanel/urls.py
+++ couchdebugpanel/urls.py
@@ -10,5 +10,5 @@ from django.conf import settings
 _PREFIX = '__debug__'
 
 urlpatterns = patterns('',
-    url(r'^%s/couch_select/$' % _PREFIX, 'couchdebugpanels.views.couch_select', name='couch_select'),
+    url(r'^%s/couch_select/$' % _PREFIX, 'couchdebugpanel.views.couch_select', name='couch_select'),
 )
